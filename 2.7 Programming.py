d18cf484d675a7c0f468caa80f044a94445d39a8
23178-stack <23178@wc.school.nz>
2026-03-11 09:39:23 +1300

    Adding in start menu and basic functions

--- 2.7 Programming.py
+++ 2.7 Programming.py
@@ -5,6 +5,21 @@
 #Variables
 
 #Functions
+def int_check(x):
+    
+
+
+
+
+def welcome():
+    print("""Welcome to the Battle of Britain!
+
+Journey through this game as we learn about some of New Zealand's unsung heros.
+There will be a series of puzzles and challenges to complete in order to win.
+""")
+
+
+
 def main():
     pass
 #Main
