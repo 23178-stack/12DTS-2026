889e352f87b6eb7fdd41f4a20756b8da104929e5
23178-stack <23178@wc.school.nz>
2026-03-17 12:30:14 +1300

    Adding menus

--- 2.7 Programming.py
+++ 2.7 Programming.py
@@ -1,26 +1,52 @@
 #Library
+import time
+import random
 
 #Constants
+MAX_HEALTH = 100
+MAX_FUEL = 100
 
 #Variables
-
+health = MAX_HEALTH
+fuel = MAX_FUEL
+name = ""
+callsign = ""
+damage_taken = 0
+welcome_choice = 0
 #Functions
-def int_check(x):
-    
+def int_check():
+    while True:
+        try:
+            x =int(input("Please enter your choice: "))
+            return x
+        except:
+            print("Please enter your choice correctly.")
+
 
 
 
 
 def welcome():
+    global welcome_choice
     print("""Welcome to the Battle of Britain!
 
 Journey through this game as we learn about some of New Zealand's unsung heros.
+
 There will be a series of puzzles and challenges to complete in order to win.
+
+Make a selection:
+[1] : Start a new game
+[2] : Instructions
+[3] : Quit Game 
 """)
-
-
-
+    welcome_choice =int_check()
+    if welcome_choice == 1:
+        print("You choose to start a new game")
+    elif welcome_choice == 2:
+        print("You choose to view the instructions")
+    else:
+        print("You choose to quit the game")
 def main():
-    pass
+    welcome()
 #Main
 main()
--- Pokemon.py
+++ Pokemon.py
@@ -1,17 +1,21 @@
 #Libraries
 import time
 import random
+import os
 #Variables
 wild_pokemon = [
     {"Name":"Tepig","Type":"Fire","Level":random.randint(1, 3),"Health":65,"Attack":["Tackle",random.randint(20,40)]},
-    {"Name":"Bewear","Type":"Fighting type","Level":random.randint(1, 3),"Health":65,"Attack":["Tackle",random.randint(20,40)]},
-    {"Name":"Tepig","Type":"Fire","Level":random.randint(1, 3),"Health":65,"Attack":["Tackle",random.randint(20,40)]}
-]
+    {"Name":"Bewear","Type":"Fighting","Level":random.randint(1, 3),"Health":130,"Attack":["Hammer In",random.randint(65,130)]},
+    {"Name":"Arceus","Type":"Normal","Level":random.randint(1, 3),"Health":220,"Attack":["Trinity Nova",random.randint(150,200)]},
+    {"Name":"Xerneas","Type":"Fairy","Level":random.randint(1, 3),"Health":210,"Attack":["Aurora Beam",random.randint(80,120)]},
+    {"Name":"Palafin","Type":"Water","Level":random.randint(1, 3),"Health":340,"Attack":["Giga Impact",random.randint(125,250)]},
+    {"Name":"Vivillon","Type":"Bug","Level":random.randint(1, 3),"Health":120,"Attack":["Bug Buzz",random.randint(65,110)]},
+    {"Name":"Heliolisk","Type":"Electric","Level":random.randint(1, 3),"Health":120,"Attack":["Powerful Bolt",random.randint(20,70)]}]
 
+own_pokemon = [{"Name":"Skeledirge","Type":"Fire","Level":random.randint(1,3),"Health":340,"Attack":["Vitality Song",random.randint(50,270)]},]
 
-
-
-own_pokemon =
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def overworld_timer():
     timer = random.randint(1, 5)
@@ -22,12 +26,15 @@ def overworld_timer():
 def battle():
     x=random.randint(0, len(wild_pokemon)-1)
     enemy_pokemon = wild_pokemon[x]
-    player_pokemom = own_pokemon
-    player_pokemom_hp = player_pokemom["Health"]
-    print("Player Pokemon:",player_pokemom["Name"])
-    print("Player pokemon HP:",player_pokemom_hp)
+    player_pokemon = own_pokemon[0]
+    player_pokemon_hp = player_pokemon["Health"]
+    print("Player Pokemon:",player_pokemon["Name"])
+    print("Player pokemon HP:",player_pokemon_hp)
     print("A wild",enemy_pokemon["Name"],"appeared")
     print("It's a", enemy_pokemon["Type"], "type pokemon")
-    print("It's a level", enemy_pokemon["Level"])
-    print("It has", enemy_pokemon["Health"])
+    print("It's a Level", enemy_pokemon["Level"], "pokemon")
+    print("It has", enemy_pokemon["Health"],"health")
 
+battle()
+clear_console()
+print("Welcome to Pokemon")
